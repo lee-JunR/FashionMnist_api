b5efb1b183636ab6ddf480b1cd69c67792c8f8b3
lee-JunR <wns2610@icloud.com>
2021-08-04 15:53:05 +0900

    ..

--- mnistapp/urls.py
+++ mnistapp/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import index, MyFileView,test
+from .views import index, MyFileView
 from . import views
 app_name = 'mnistapp'
 
--- mnistapp/views.py
+++ mnistapp/views.py
@@ -19,19 +19,23 @@ def index(request):
 
         # 이미지 업로드
         img = cv2.imdecode(np.fromstring(request.FILES['image'].read(), np.uint8), cv2.IMREAD_GRAYSCALE)
-
+        request.FILES['image'].seek(0)
         # 28x28로 변환
         img = cv2.resize(img, dsize=(28, 28), interpolation=cv2.INTER_AREA)
         img2 = cv2.resize(img, dsize=(280, 280), interpolation=cv2.INTER_AREA)
         img = 1-img/255.0
-        cv2.imshow('img', img2)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        # cv2.imshow('img', img2)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
         #모델 불러오기
         model = load_model('mnistapp/kerasmodel/model.h5')
         # Model 사용하기
         prediction = model.predict(img.reshape(-1, 28, 28, 1))
         label = class_name[np.argmax(prediction)]
+        request.data['title'] = label
+        serializer = image_serializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
         return render(request, "mnistapp/insert_img.html", {"predictions": label})
     else:
         return render(request, "mnistapp/insert_img.html")
